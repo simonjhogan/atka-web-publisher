ce273ce970207467d24a76953bc9525d25ef0479
Simon J Hogan <simon.hogan@iinet.net.au>
2014-07-24 23:06:31 +1000

    Update licence details

--- WebContent/cms/php/_vti_cnf/database.class.php
+++ WebContent/cms/php/_vti_cnf/database.class.php
@@ -1,17 +1,17 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TW|04 Aug 2012 23:49:46 -0000
+vti_timelastmodified:TR|24 Jul 2014 13:04:42 -0000
 vti_author:SR|TRINITY\\simon
 vti_modifiedby:SR|TRINITY\\simon
 vti_nexttolasttimemodified:TW|04 Aug 2012 23:49:46 -0000
 vti_timecreated:TR|24 Jul 2014 12:59:33 -0000
-vti_cacheddtm:TX|24 Jul 2014 12:59:33 -0000
-vti_filesize:IR|733
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_syncwith_h3consulting.net\:21/%2fpublic_html:TW|24 Jul 2014 12:59:33 -0000
+vti_syncofs_h3consulting.net\:21/%2fpublic_html:TW|04 Aug 2012 23:49:46 -0000
+vti_cacheddtm:TX|24 Jul 2014 13:04:42 -0000
+vti_filesize:IR|1355
 vti_cachedneedsrewrite:BR|false
 vti_cachedhasbots:BR|false
 vti_cachedhastheme:BR|false
 vti_cachedhasborder:BR|false
 vti_charset:SR|utf-8
-vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|
-vti_syncwith_h3consulting.net\:21/%2fpublic_html:TW|24 Jul 2014 12:59:33 -0000
-vti_syncofs_h3consulting.net\:21/%2fpublic_html:TW|04 Aug 2012 23:49:46 -0000
